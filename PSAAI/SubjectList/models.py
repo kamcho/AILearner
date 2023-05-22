6b0dafe2b4344d5f888d885ba3d91bcf607c795d
kamcho <njokevin9@gmail.com>
2023-05-22 22:59:11 +0300

    academics

--- PSAAI/SubjectList/models.py
+++ PSAAI/SubjectList/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+
+from django.db import models
+import uuid
+
+from Users.models import MyUser
+
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    grade= models.CharField(max_length=2,default="1")
+
+    def __str__(self):
+        return self.name + ' ' + self.grade
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    grade= models.CharField(max_length=2,default="1")
+
+    def __str__(self):
+        return self.name + ' ' + self.grade
+class MySubjects(models.Model):
+    name = models.ManyToManyField(Course,blank=True,related_name='my_subjects')
+    user = models.OneToOneField(MyUser,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+#
+class Topic(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Subtopic(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
