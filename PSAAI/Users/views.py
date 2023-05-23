8e7551b187c6a588d43074f6ac99e092d0772125
kamcho <njokevin9@gmail.com>
2023-05-24 01:31:56 +0300

    profile update capability

--- PSAAI/Users/views.py
+++ PSAAI/Users/views.py
@@ -1,5 +1,5 @@
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.urls import reverse
@@ -28,6 +28,26 @@ class MyProfile(DetailView):
         context['subjects'] = MySubjects.objects.filter(user=self.request.user)
 
         return context
+    def post(self, request, *args, **kwargs):
+        profile=PersonalProfile.objects.get(user=self.request.user)
+        fname = request.POST.get('first-name')
+        new_phone_number = request.POST.get('phone_number')
+        lname = request.POST.get('last-name')
+        country = request.POST.get('country')
+        city = request.POST.get('city')
+        area = request.POST.get('area')
+
+        # Check if the password is correct
+        profile.phone = new_phone_number
+        profile.f_name=fname
+        profile.l_name=lname
+        profile.country=country
+        profile.city=city
+        profile.area=area
+        profile.save()
+
+        # messages.success(request, 'Phone number updated successfully')
+        return redirect(request.get_full_path())
 
 class Home(TemplateView):
     template_name = 'Users/home.html'
