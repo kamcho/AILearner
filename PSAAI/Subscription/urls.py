a0bdd7a7feb23813c2685640886d21acecf7d9d4
kamcho <njokevin9@gmail.com>
2023-08-09 03:45:23 +0300

    mpesa payment

--- PSAAI/Subscription/urls.py
+++ PSAAI/Subscription/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
 
     path('Subscription/', Subscribe.as_view(), name='subscription'),
-    path('Card-Payment/', StripeCard.as_view(), name='stripe-pay'),
-    path('process-payment/', views.StripeWebhookView, name='process-payment'),
+    # path('Card-Payment/', StripeCard.as_view(), name='stripe-pay'),
+    path('process-payment/', views.initiate_payment, name='process-payment'),
+    path('callback/', views.payment_callback, name='callback')
+
 
 ]
--- PSAAI/Subscription/views.py
+++ PSAAI/Subscription/views.py
@@ -1,11 +1,19 @@
 import datetime
+import json
 from datetime import timedelta
+# views.py
+import requests
+from django.shortcuts import render, redirect
 
+import base64
+import hashlib
+import datetime
 import stripe
 from django.db import DatabaseError, IntegrityError
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
+from requests.auth import HTTPBasicAuth
 from stripe.error import StripeError
 
 from SubjectList.models import PaymentNotifications
@@ -32,173 +40,78 @@ class Subscribe(TemplateView):
             pass
 
 
-class StripeCard(TemplateView):
-    template_name = 'Subscription/card.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(StripeCard, self).get_context_data(**kwargs)
+def generate_access_token():
+    consumer_key = '3r0PGbGzT0q4XX3aLnBHeyZrO9QlGnyx'
+    consumer_secret = 'VbgYH9GK5GibFruV'
+    api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
 
-        try:
-            subs = Subscriptions.objects.all()
+    response = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
 
-            context['subs'] = subs
-            kids = PersonalProfile.objects.filter(ref_id=self.request.user.uuid)
-            context['kids'] = kids
+    if response.status_code == 200:
+        data = response.json()
+        access_token = data.get('access_token')
+        return access_token
+    else:
+        return JsonResponse({'error': 'Token generation failed'}, status=response.status_code)
 
-            return context
 
-        except DatabaseError:
-            pass
+    return HttpResponse(status=405)
 
-    def post(self, request, *args, **kwargs):
-        stripe.api_key = 'sk_test_51MrhGPHSDxMMHnYTxwz5LLK9vGRHde981TLoCjmE9HNOmtbvAlIZbn9eCk29JFq98zziGrwKOxfj1ol5N9TDEOHo00eHUdjtjw'
-        if request.method == "POST":
-            card_number = request.POST.get("card")
-            exp_month = request.POST.get("month")
-            exp_year = request.POST.get("year")
-            cvc = request.POST.get("cvc")
-            names = request.POST.get('names')
-            selected = request.POST.getlist('selected_kids')
-            type = request.POST.get('subscription')
-            amount = request.POST.get('amount')
-            try:
-                token = stripe.Token.create(
-                    card={
-                        'number': card_number,
-                        'exp_month': exp_month,
-                        'exp_year': exp_year,
-                        'cvc': cvc,
-                        "name": names,
-                    },
-                )
 
-                customer = stripe.Customer.create(
-                    source=token, email=request.user, name=names
-                )
-                amount = int(amount) * 100
-                charge = stripe.Charge.create(
-                    amount=amount,
-                    currency='kes',
-                    customer=customer.id,
-                    description="Test payment",
-                    metadata={'students': ', '.join(selected),
-                              'user': request.user,
-                              'type': type,
-                              },
-                )
-                return self.render_to_response({"success": True})
+def generate_mpesa_password(paybill_number):
 
-            except StripeError as e:
-                pass
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    consumer_key = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+    concatenated_string = f"{paybill_number}{consumer_key}{timestamp}"
+    base64_encoded = base64.b64encode(concatenated_string.encode()).decode('utf-8')
+
+    return base64_encoded
+
+
+def initiate_payment(request):
+    # Use your API credentials here
+    consumer_key = 'uBAiPdLXOGEmOqjBYlh2Wt57ecTaRlyz'
+    consumer_secret = 'rR5Ftd45gurgBaP2'
+    paybill = "174379"
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+
+    access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+
+    access_token = generate_access_token()
+    password = generate_mpesa_password(paybill)
+
+    payment_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
+    payload = {
+        "BusinessShortCode": paybill,
+        "Password": password,  # Generate password as per API documentation
+        "Timestamp": timestamp,  # Use current timestamp
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": "1",
+        "PartyA": "254711371265",
+        "PartyB": paybill,
+        "PhoneNumber": "254711371265",
+        "CallBackURL": "https://81fb-105-163-157-153.ngrok-free.app/Subscription/callback/",
+        "AccountReference": "E-Learning Subscription",
+        "TransactionDesc": "pay 1ks to e-learner"
+    }
+
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(payment_url, json=payload, headers=headers)
+    return HttpResponse(response)
+
 
 
 @csrf_exempt
-def StripeWebhookView(request):
-    # @csrf_protect
+def payment_callback(request):
 
-    if request.method == "POST":
-        payload = request.body
+    data = request.body.decode('utf-8')
+    data = json.loads(data)
+    print(data)
 
-        sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
-        endpoint_secret = "whsec_WRS1nWAj4LEIt9vFZ0tMOM7bkynvjJlr"
-
-        try:
-            event = stripe.Webhook.construct_event(
-                payload, sig_header, endpoint_secret
-            )
-
-
-        except stripe.error.SignatureVerificationError:
-            return HttpResponse(status=400)
-        except ValueError:
-            print(payload)
-            return HttpResponse(status=400)
-
-        if event['type'] == 'charge.failed':
-            charge = event['data']['object']
-            print(charge)
-        elif event['type'] == 'charge.pending':
-            charge = event['data']['object']
-            print(charge)
-        elif event['type'] == 'charge.succeeded':
-            charge = event['data']['object']
-            print(charge['metadata'])
-            amount = charge['amount']
-            students = charge['metadata']['students']
-            transact_id = charge['id']
-            brand = charge['payment_method_details']['card']['brand']
-            currency = charge['currency']
-            country = charge['payment_method_details']['card']['country']
-            name = charge['billing_details']['name']
-            created = charge['created']
-            user = charge['metadata']['user']
-            sub = charge['metadata']['type']
-            print(type(amount))
-            print("\n\n\n\n\n\n\n")
-
-            try:
-                user = MyUser.objects.get(email=user)
-                payment = StripeCardPayments.objects.create(
-                    user=user,
-                    amount=amount,
-
-                    transact_id=transact_id,
-                    student_list=students,
-                    type=sub,
-
-                    currency=currency,
-                    name=name,
-                    country=country,
-                    brand=brand,
-                    created=created
-                )
-                payment.save()
-                update_subscription(students, sub)
-                notify(user=user,amount=amount, beneficiaries=students, subscription_type=sub)
-
-            except MyUser.MultipleObjectsReturned:
-                pass
-            except MyUser.DoesNotExist:
-                pass
-            except IntegrityError:
-                pass
-            except DatabaseError:
-                pass
-        else:
-            print('Unhandled event type {}'.format(event['type']))
-        return HttpResponse(status=200)
-
-
-
-
-def update_subscription(users, sub_type):
-    email_list = [email.strip() for email in users.split(",")]
-    print(type, '\n\n\n\n\n\n')
-
-    for user in email_list:
-        try:
-            myuser = MyUser.objects.get(email=user)
-            subscribe = MySubscription.objects.get(user=myuser)
-            subs = Subscriptions.objects.get(type=sub_type.title())
-
-            subscribe.type = subs
-            subscribe.status = True
-            expiry = subscribe.expiry + timedelta(days=30)
-            subscribe.expiry = expiry
-            subscribe.date = datetime.date.today()
-            subscribe.save()
-
-        except MyUser.MultipleObjectsReturned:
-            pass
-        except MySubscription.MultipleObjectsReturned:
-            pass
-
-
-
-    return None
-
-
-def notify(user, amount, beneficiaries, subscription_type):
-    notification = PaymentNotifications.objects.create(user=user, beneficiaries=beneficiaries, amount=amount,
-                                                       subscription_type=subscription_type)
-    return None
+    return JsonResponse({'response':data})
