36803c049a79f1f9ed4ecdafc6acab7b6db19947
kamcho <61869599+kamcho@users.noreply.github.com>
2023-10-14 01:23:07 +0300

    Update settings.py

--- PSAAI/PSAAI/settings.py
+++ PSAAI/PSAAI/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-*1r4hwdyal=y_j%*m&+-_4!@j)33!9a(z*k_%71c($@&71fbue
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '33a2-196-110-128-186.ngrok-free.app']
-CSRF_TRUSTED_ORIGINS = ["https://33a2-196-110-128-186.ngrok-free.app"]
+ALLOWED_HOSTS = ['127.0.0.1','*', '33a2-196-110-128-186.ngrok-free.app']
+CSRF_TRUSTED_ORIGINS = ["https://33a2-196-110-128-186.ngrok-free.app",'*']
 
 # Application definition
 
