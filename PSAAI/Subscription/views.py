1809de1bda17cb2c86a4aadfb2aa64db8ffa6519
kamcho <njokevin9@gmail.com>
2023-10-24 03:05:47 +0300

    stk 2

--- PSAAI/Subscription/views.py
+++ PSAAI/Subscription/views.py
@@ -41,15 +41,14 @@ class Subscribe(LoginRequiredMixin, TemplateView):
 
 
 def generate_access_token():
-    consumer_key = '3r0PGbGzT0q4XX3aLnBHeyZrO9QlGnyx'
-    consumer_secret = 'VbgYH9GK5GibFruV'
-    api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
-
-    response = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
-
+    response = requests.request("GET", 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', headers = { 'Authorization': 'Basic cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ=='})
     if response.status_code == 200:
-        data = response.json()
-        access_token = data.get('access_token')
+        access_token = response.text.encode('utf8')
+        data = json.loads(access_token)
+        print(data)
+
+    # Access the "access_token" value
+        access_token = data["access_token"]
         return access_token
     else:
         return JsonResponse({'error': 'Token generation failed'}, status=response.status_code)
@@ -63,40 +62,45 @@ def generate_mpesa_password(paybill_number):
     concatenated_string = f"{paybill_number}{consumer_key}{timestamp}"
     base64_encoded = base64.b64encode(concatenated_string.encode()).decode('utf-8')
 
-    return base64_encoded
+    return str(base64_encoded)
 
 
 def initiate_payment(request):
-    paybill = "174379"
+    paybill = 174379
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
     access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
 
     access_token = generate_access_token()
+    
     password = generate_mpesa_password(paybill)
-
-    payment_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
-    payload = {
-        "BusinessShortCode": paybill,
-        "Password": password,  # Generate password as per API documentation
-        "Timestamp": timestamp,  # Use current timestamp
-        "TransactionType": "CustomerPayBillOnline",
-        "Amount": "1",
-        "PartyA": "254711371265",
-        "PartyB": paybill,
-        "PhoneNumber": "254711371265",
-        "CallBackURL": "https://81fb-105-163-157-153.ngrok-free.app/Subscription/callback/",
-        "AccountReference": "E-Learning Subscription",
-        "TransactionDesc": "pay 1ks to e-learner"
-    }
+    # print(access_token, 'and', password)
 
     headers = {
-        "Authorization": f"Bearer {access_token}",
-        "Content-Type": "application/json"
-    }
+  'Authorization': 'Bearer 52hMh3GrObSACC7LCrxyvuSUutih'
+}
 
-    response = requests.post(payment_url, json=payload, headers=headers)
-    return HttpResponse(response)
+    payload = {
+    "BusinessShortCode": 174379,  # Use double quotes for all keys and values
+    "Password": password,  # Use the generated password
+    "Timestamp": timestamp,
+    "TransactionType": "CustomerPayBillOnline",
+    "Amount": 1,
+    "PartyA": 254722985477,
+    "PartyB": 174379,
+    "PhoneNumber": 254722985477,
+    "CallBackURL": "http://16.170.98.188:8000/Subscription/callback",
+    "AccountReference": "CompanyXLTD",
+    "TransactionDesc": "Payment of X"
+}
+
+
+    responses = requests.request("POST", 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest', headers = headers, json = payload)
+    # print(responses.text)
+    print('after request')
+    # print(responses)
+    print('post requeat')
+    return HttpResponse(responses.text)
 
 
 @csrf_exempt
